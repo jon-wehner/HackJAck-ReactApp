4b9937272eaac7753a22539b438566f90e77a226
Jon Wehner <jonjwehner@gmail.com>
2021-08-10 23:18:19 -0700

    removed test components

--- src/hello.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var React = require("react");
-var greet = function (_a) {
-    var person = _a.person, date = _a.date;
-    return React.createElement("h1", null, "Hi, " + person + ", today is " + date);
-};
--- src/hello.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-
-type greetProps = {
-  person: string;
-  date: Date;
-}
-const greet = ({ person, date }: greetProps) => <h1>{`Hi, ${person}, today is ${date}`}</h1>
-
