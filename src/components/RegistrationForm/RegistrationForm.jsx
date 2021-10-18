826beca392672b150a8c370d9fc7e959e05ea274
Jon Wehner <jonjwehner@gmail.com>
2021-10-18 11:53:47 -0700

    initialize reg form componenrt

---
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const RegistrationForm = () => {
+  const [email, setEmail] = useState('');
+  return (
+    <form>
+      <input></input>
+    </form>
+  );
+};
