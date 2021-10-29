5af690e0ef1a4f281592374060fa7ac723a916db
Jon Wehner <jonjwehner@gmail.com>
2021-10-28 20:45:15 -0700

    added registration fetch and username field

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -4,12 +4,32 @@ const RegistrationForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [username, setUsername] = useState('');
   const [name, setName] = useState('');
+
+  const register = async (e) => {
+    e.preventDefault();
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/JSON',
+      },
+      body: JSON.stringify({
+        email, username, confirmPassword, password,
+      }),
+    };
+    await fetch(`${process.env.REACT_APP_API_URI}/auth/register`, options);
+  };
+
   return (
-    <form>
+    <form onSubmit={register}>
       <label htmlFor="email">
         Email:
         <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <label htmlFor="username">
+        Username:
+        <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
       </label>
       <label htmlFor="name">
         Name:
