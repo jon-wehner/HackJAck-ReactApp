caf4c3c9b4a8e6b94ae94091a7a4d5268da69105
Jon Wehner <jonjwehner@gmail.com>
2021-11-02 23:25:28 -0700

    moved browser router to app js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
 import LoginForm from './components/LoginForm/LoginForm';
 import RegistrationForm from './components/RegistrationForm/RegistrationForm';
@@ -7,18 +7,20 @@ import RegistrationForm from './components/RegistrationForm/RegistrationForm';
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route path="/" exact>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </Route>
-        <Route path="/login">
-          <LoginForm />
-        </Route>
-        <Route path="/register">
-          <RegistrationForm />
-        </Route>
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </Route>
+          <Route path="/login">
+            <LoginForm />
+          </Route>
+          <Route path="/register">
+            <RegistrationForm />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,9 +8,7 @@ import { StateProvider } from './store';
 ReactDOM.render(
   <React.StrictMode>
     <StateProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </StateProvider>
   </React.StrictMode>,
   document.getElementById('root'),
